3ae70e02ee08d2d533a90eed6cd15ec074232db6
Maite <maitechu87@gmail.com>
2019-11-20 18:27:29 +0100

    Revert "WIP: probando fragments"
    This reverts commit 065e6feaf1cf27bfa05903975cabbeae050ef377.

--- app/src/main/java/com/example/signish/MainActivity.java
+++ app/src/main/java/com/example/signish/MainActivity.java
@@ -1,29 +1,31 @@
 package com.example.signish;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.example.signish.Vista.LoginFragment;
-
 public class MainActivity extends AppCompatActivity {
-
-    FragmentManager fragManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //instanciar el fragment
-        fragManager = getSupportFragmentManager();
+        final EditText usuario = findViewById(R.id.usuarioTxt);
+        final EditText password = findViewById(R.id.passwordTxt);
+        final Button botonEntrar = findViewById(R.id.botonEntrar);
 
-        //usar replace porque add unicamente es para cuando esta vacio
-        fragManager.beginTransaction().replace(R.id.fragmentLogin,new LoginFragment()).commit();
+        botonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/signish/VistaModelo/LoginVistaModelo.java
+++
@@ -1,8 +0,0 @@
-package com.example.signish.VistaModelo;
-
-import androidx.lifecycle.ViewModel;
-
-public class LoginVistaModelo extends ViewModel {
-
-
-}
