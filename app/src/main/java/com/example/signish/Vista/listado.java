4804ce7f642febe76cf49a64535dcfa2e3cc3de8
Maite <maitechu87@gmail.com>
2019-12-05 19:16:29 +0100

    WIP Estructura RecyclerView

--- app/src/main/java/com/example/signish/Vista/listado.java
+++ app/src/main/java/com/example/signish/Vista/listado.java
@@ -7,15 +7,28 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.signish.Modelo.Usuario;
 import com.example.signish.R;
 import com.example.signish.VistaModelo.ListadoViewModel;
 
+import java.util.List;
+
+
 public class listado extends Fragment {
+
+    //declarar variables, luego se le asignara el id
+
+    RecyclerView recyclerView;
+
+    //declarar variable del fichero vistamodelo
+    ListadoViewModel listadoVistaModelo;
 
     private ListadoViewModel mViewModel;
 
@@ -26,7 +39,13 @@ public class listado extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.listado_fragment, container, false);
+
+        //declarar variable y pegar el return
+        View interfazListado = inflater.inflate(R.layout.listado_fragment, container, false);
+        recyclerView = interfazListado.findViewById(R.id.noName);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return interfazListado;
     }
 
     @Override
@@ -34,6 +53,43 @@ public class listado extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ListadoViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+        // lista con los fichajes
+        List<Usuario> fichajes;
+
+        public UserAdapter(List<Usuario> fichajes) {
+            this.fichajes = fichajes;
+        }
+
+        @NonNull
+        @Override
+        public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class UserViewHolder extends RecyclerView.ViewHolder{
+
+            //declarar variables
+
+            public UserViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                //aquí poner lo que se tiene que mostrar (variable/id)
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/signish/VistaModelo/ListadoViewModel.java
+++ app/src/main/java/com/example/signish/VistaModelo/ListadoViewModel.java
@@ -1,7 +1,18 @@
 package com.example.signish.VistaModelo;
 
+import android.content.Context;
+
+import com.example.signish.Repository;
+
 import androidx.lifecycle.ViewModel;
 
 public class ListadoViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    Repository miRepositorio = Repository.get();
+
+    public void setContext(Context context){
+        miRepositorio.setContext(context);
+    }
+
+    //funciones a partir de instrucciones de la vista
+
 }
