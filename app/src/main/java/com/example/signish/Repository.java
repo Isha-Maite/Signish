c5502e82ab7297682ed92dd1d78954784a26d79b
Maite <maitechu87@gmail.com>
2020-01-15 19:54:56 +0100

    //Login using SQLite

--- app/src/main/java/com/example/signish/Repository.java
+++ app/src/main/java/com/example/signish/Repository.java
@@ -1,6 +1,8 @@
 package com.example.signish;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import com.example.signish.Model.Fichaje;
@@ -9,7 +11,6 @@ import com.example.signish.Model.Usuario;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -23,6 +24,9 @@ public class Repository {
     private static Repository repositorio;
     private Context context;
     private File file = null;
+
+    String userSq;
+    String passSq;
 
 
     //Se genera constructor en privado para que no se haga más de un repositorio
@@ -43,45 +47,32 @@ public class Repository {
 
 
     //Método para verificar usuario correcto
+    public Boolean userOk(String user, String password) {
+
+        UsuarioDbHelper admin = new UsuarioDbHelper(context);
+        SQLiteDatabase db = admin.getReadableDatabase();
+        //se crea objeto cursor para guardar los datos de la consulta
+        Cursor cursor = db.rawQuery("SELECT nombre, contrasenya FROM usuarios", null);
+
+        //se recorren los datos con un while
+        while(cursor.moveToNext()){
+            userSq = cursor.getString(cursor.getColumnIndex("nombre"));
+            passSq = cursor.getString(cursor.getColumnIndex("contrasenya"));
+            if(user.equals(userSq)&&password.equals(passSq)){
+                Log.i("lala","Login con QLIte");
+                cursor.close();
+                return true;
+            }
+        } cursor.close();
+          return false;
 
 
-    public Boolean userOk(String user, String password) throws IOException, ClassNotFoundException {
-
-        Usuario usuario;
-
-        FileInputStream filein = context.openFileInput("userFile.dat");
-        ObjectInputStream ois = new ObjectInputStream(filein);
-
-        try {
-            do {
-                usuario = (Usuario)ois.readObject();
-                Log.i("PROBANDO", usuario.getUsuario()+usuario.getPassword());
-                if (user.equals(usuario.getUsuario()) && password.equals(usuario.getPassword())) {
-                    ois.close();
-                    Log.i("LALALA", "He hecho login");
-                    return true;
-                } else {
-                    ois.close();
-                    return false;
-                }
-            }while(usuario != null);
-        } catch (EOFException eo){
-            System.out.println ("Fin");
-        }
-        ois.close();
-        return false;
     }
 
-
-    public void createUser(Usuario usuario){
-
-    }
 
     /*método para guardar fichajes, modificado para que si el fichero existe no añada la cabecera,
     Una cabecera que se genera sola en el fichero.
-    Se sobreescribe el método writeStreamHeader
-     */
-
+    Se sobreescribe el método writeStreamHeader*/
     public void createEntry() {
 
         ObjectOutputStream oos;
